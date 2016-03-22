fdff6d4f9d0a94445802d687b40fb8063e8e963b
Hunter O'Rourke <orourke.hunter@gmail.com>
2016-03-22 16:00:14 -0400

    Added All Pages
    Added default html pages for maps, courses, and settings.
    Added default css files for maps, coruses, and settings
    Added example page switch function in index.js

--- TheStudy/www/js/index.js
+++ TheStudy/www/js/index.js
@@ -47,3 +47,17 @@ var app = {
         console.log('Received Event: ' + id);
     }
 };
+
+
+function loadSettingsPage()
+{
+	window.location = "settings.html"
+}
+function loadMapsPage()
+{
+	window.location = "maps.html"
+}
+function loadCoursesPage()
+{
+	window.location = "courses.html"
+}
