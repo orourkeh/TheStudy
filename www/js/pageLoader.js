06af85226351488d022f0dc19cd41d2ef22a2496
Hunter O'Rourke <hdor222@uky.edu>
2016-04-16 16:11:52 -0400

    Changed file structure for packaging

---
+++ www/js/pageLoader.js
@@ -0,0 +1,51 @@
+/*
+file decriptor
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
+
+function loadHomePage()
+{
+	window.location = "index.html"
+}
+function loadSettingsPage()
+{
+	window.location = "settings.html"
+}
+function loadMapsPage()
+{
+	window.location = "maps.html"
+}
+function loadCoursesPage()
+{
+	window.location = "courses.html"
+}
