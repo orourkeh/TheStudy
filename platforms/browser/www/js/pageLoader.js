aeb53c0cb8b4b53ea1ae2ca1805921a459ed2e93
Hunter O'Rourke <hdor222@uky.edu>
2016-04-28 20:31:59 -0400

    Finished?

--- platforms/browser/www/js/pageLoader.js
+++ platforms/browser/www/js/pageLoader.js
@@ -1,40 +1,9 @@
 /*
 file decripto
 
-
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    }
-};
-r
  */
+ 
+ 
 var app = {
     // Application Constructor
     initialize: function() {
@@ -52,7 +21,7 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-		document.addEventListener('deviceready', function() {navigator.splashscreen.hide();});
+		document.addEventListener('deviceready', function() {navigator.splashscreen.hide();}, false);
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
@@ -67,6 +36,7 @@ var app = {
     }
 };
 
+//Changes current window view to respective pages
 function loadHomePage()
 {
 	window.location = "index.html"
